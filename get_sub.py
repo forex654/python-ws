1e57eb7120b5bd281cf74763475fe94089b9ee9e
forex654 <forex654@gmail.com>
2026-03-07 12:40:47 +0800

    Update get_sub.py

--- get_sub.py
+++ get_sub.py
@@ -1,5 +1,5 @@
 import hmac, hashlib
-UUID = 'cbc0fec5-b992-44a3-b97c-d943c5378e08'
+UUID = 'f755dc04-88ff-4348-bd5d-3f1bbad22abb'
 sub = hmac.new(UUID.encode(), b'sub', hashlib.sha256).hexdigest()[:10]
 ws = hmac.new(UUID.encode(), b'ws', hashlib.sha256).hexdigest()[:10]
 print(f"订阅地址: https://你的域名/{sub}")
